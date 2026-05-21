28006912c66010459ea2ed41cb61fa87f712a6f7
Tabeerimran063 <tabeerimran063@gmail.com>
2026-05-21 17:45:00 +0500

    new page

---
+++ our.js
@@ -0,0 +1,17 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+    reveals.forEach((element) => {
+
+        const windowHeight = window.innerHeight;
+        const revealTop = element.getBoundingClientRect().top;
+        const revealPoint = 120;
+
+        if (revealTop < windowHeight - revealPoint) {
+            element.classList.add("active");
+        }
+
+    });
+
+});
