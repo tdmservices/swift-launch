c1cd1b5c5ecc6e97e0d648b2edda3a1186ede031
tdmservices <tdm.services2023@gmail.com>
2026-06-04 19:20:03 +0500

    hmm

--- E-commerce/E-commerce.js
+++ E-commerce/E-commerce.js
@@ -115,7 +115,7 @@ const overlay = document.getElementById('popupOverlay');
 const openBtn = document.getElementById('openPopupBtn');
 
 // ✅ Yeh add karo
-document.querySelectorAll('.primary-btn, .btn-secondary, .btn-primary').forEach(btn => {
+document.querySelectorAll('.primary-btn, .btn-secondary, .btn-primary, .call-btn, .schedule-btn, .work-call-btn, .transparent-btn').forEach(btn => {
     btn.addEventListener('click', () => {
         overlay.classList.add('active');
         formContainer.style.display = 'flex';
